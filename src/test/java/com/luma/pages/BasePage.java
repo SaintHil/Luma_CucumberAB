57c263dbf28b2f49203937ff03385e7b8239dc14
ocolak95 <ocolak95@hotmail.com>
2023-12-24 16:56:55 +0100

    first commit

---
+++ src/test/java/com/luma/pages/BasePage.java
@@ -0,0 +1,12 @@
+package com.luma.pages;
+
+
+import com.luma.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class BasePage {
+    public BasePage() {
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+}
