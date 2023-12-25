f51fa3a7f450ac2cec1e7665a5a32ba6cb80239a
NesibeOguz <n.asutay@hotmail.com>
2023-12-25 12:42:34 +0300

    login created

--- src/test/java/com/luma/pages/BasePage.java
+++ src/test/java/com/luma/pages/BasePage.java
@@ -2,11 +2,15 @@ package com.luma.pages;
 
 
 import com.luma.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public abstract class BasePage {
     public BasePage() {
         PageFactory.initElements(Driver.get(), this);
     }
+    @FindBy(xpath = "//a[contains(.,'Sign In')]")
+    public WebElement signInLink;
 
 }
